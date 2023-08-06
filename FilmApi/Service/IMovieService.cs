372f1a86c7b9d8a7bcd399cd8916828086fe0c12
maroine <maroine.cherif@outlook.fr>
2023-08-07 00:08:16 +0200

    add services

---
+++ FilmApi/Service/IMovieService.cs
@@ -0,0 +1,12 @@
+﻿using FilmApi.Models;
+
+namespace FilmApi.Service
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<Movie>> GetAllMoviesAsync();
+        void AddOrRemoveFavorite(int userId, int movieId, bool addToFavorites);
+        void MarkMovieAsWatched(int userId, int movieId);
+        List<Movie> GetFavoriteMovies(int userId, string sortBy);
+    }
+}
---
+++ FilmApi/Service/Implementation/MovieService.cs
@@ -0,0 +1,81 @@
+﻿using FilmApi.DbContexts;
+using FilmApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmApi.Service.Implementation
+{
+    public class MovieService : IMovieService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public MovieService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Movie>> GetAllMoviesAsync()
+        {
+            return await _dbContext.Movies.ToListAsync();
+        }
+
+        public void AddOrRemoveFavorite(int userId, int movieId, bool addToFavorites)
+        {
+            var userMovie = _dbContext.UserMovies.FirstOrDefault(um => um.UserId == userId && um.MovieId == movieId);
+
+            if (userMovie == null)
+            {
+                userMovie = new UserMovie
+                {
+                    UserId = userId,
+                    MovieId = movieId,
+                    IsFavorite = addToFavorites,
+                    HasWatched = false,
+                    WatchedDate = DateTime.MinValue
+                };
+                _dbContext.UserMovies.Add(userMovie);
+            }
+            else
+            {
+                userMovie.IsFavorite = addToFavorites;
+            }
+
+            _dbContext.SaveChanges();
+        }
+
+        public void MarkMovieAsWatched(int userId, int movieId)
+        {
+            var userMovie = _dbContext.UserMovies.FirstOrDefault(um => um.UserId == userId && um.MovieId == movieId);
+
+            if (userMovie != null)
+            {
+                userMovie.HasWatched = true;
+                userMovie.WatchedDate = DateTime.Now;
+
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public List<Movie> GetFavoriteMovies(int userId, string sortBy)
+        {
+            IQueryable<Movie> query = _dbContext.Movies;
+
+            var userFavorites = _dbContext.UserMovies
+                .Where(um => um.UserId == userId && um.IsFavorite)
+                .Select(um => um.MovieId);
+
+            query = query.Where(movie => userFavorites.Contains(movie.Id));
+
+            if (sortBy == "releaseDate")
+            {
+                query = query.OrderBy(movie => movie.ReleaseDate);
+            }
+            else if (sortBy == "rating")
+            {
+                query = query.OrderByDescending(movie => movie.Rating);
+            }
+
+            return query.ToList();
+        }
+    }
+
+}
